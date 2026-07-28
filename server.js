71c30ead82bea5579a8dc47d66d078fe6ae74d06
Jaykumar7r <jayravuri7@gmail.com>
2026-07-28 21:35:48 +0530

    Fix Render deployment

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.get("/images", (req, res) => {
   });
 });
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on port 3000");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
