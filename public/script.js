03dbd0cad903aa6372a580d18253566599d3d6f4
Jaykumar7r <jayravuri7@gmail.com>
2026-07-29 17:23:58 +0530

    Fixed script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-async function loadImages(){
+async function loadImages() {
 
     const response = await fetch("/images");
     const images = await response.json();
@@ -11,49 +11,11 @@ async function loadImages(){
 
         gallery.innerHTML += `
         <div class="card">
-
-            <img src="/uploads/${image}">
-
+            <img src="/uploads/${image}" alt="${image}">
             <br><br>
-
             <a href="/uploads/${image}" download>
                 <button>Download</button>
             </a>
-
-        </div>
-        `;
-
-    });
-
-}
-
-loadImages();
-
-    loadImages();
-});
-
-async function loadImages(){
-
-    const response = await fetch("/images");
-    const images = await response.json();
-
-    const gallery = document.getElementById("gallery");
-
-    gallery.innerHTML = "";
-
-    images.forEach(image => {
-
-        gallery.innerHTML += `
-        <div class="card">
-
-            <img src="/uploads/${image}">
-
-            <br><br>
-
-            <a href="/uploads/${image}" download>
-                <button>Download</button>
-            </a>
-
         </div>
         `;
 
